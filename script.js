c278edea57e656f512fa585c2a717c4985d57c17
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-01-18 18:13:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector('.navbar');
+let icons = document.querySelector('.fab-icons');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
+    icons.classList.toggle('active')
 }
