c68d5d70490f222fd6ab0b435281ac6e8f73bd72
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-02-03 02:20:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector("#menu-bar");
-let navbar = document.querySelector('.navbar');
-let icons = document.querySelector('.fab-icons');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-    icons.classList.toggle('active')
-}
