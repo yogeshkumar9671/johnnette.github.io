606b7604993e310935c885c474985a5830c7cb57
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-02-09 18:51:43 +0530

    Delete static/app directory

--- static/app/js/script.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector("#menu");
-let navbar = document.querySelector(".navbar");
-let btn = document.querySelector(".user");
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-    btn.classList.toggle('active')
-}
