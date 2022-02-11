ce07a6e1edd64bf039b73a1409025099214efd34
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2022-02-11 18:40:14 +0530

    Add files via upload

---
+++ app/js/home.js
@@ -0,0 +1,12 @@
+// Initialize Swiper
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
